b5498e161baa3e1221fa480adda2acee603e7d6b
MauroMeli23 <mauro.gil@mercadolibre.com>
2023-09-11 12:27:39 -0300

    Domain Driven Design structure

--- cmd/server/handler/product.go
+++ cmd/server/handler/product.go
@@ -1,1 +1,62 @@
 package handler
+
+import (
+	"github.com/MauroMeli23/api-supermarket/internal/domain"
+	"github.com/MauroMeli23/api-supermarket/internal/product"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func GetAllProducts(c *gin.Context, products []domain.Product) {
+	c.JSON(http.StatusOK, products)
+}
+
+func GetProductByID(c *gin.Context, products []domain.Product) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID de producto no válido"})
+		return
+	}
+
+	p, err := product.GetProductByID(id, products)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Producto no encontrado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, p)
+}
+
+func GetProductByName(c *gin.Context, products []domain.Product) {
+	name := c.Query("name")
+	if name == "" {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Debe ingresar un nombre"})
+		return
+	}
+
+	p, err := product.GetProductByName(name, products)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Producto no encontrado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, p)
+}
+
+func AddNewProduct(c *gin.Context, products *[]domain.Product) error {
+	var newProduct domain.Product
+
+	if err := c.BindJSON(&newProduct); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return err
+	}
+
+	err := product.AddNewProduct(newProduct, products)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return err
+	}
+
+	return nil
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,47 @@
-package server
+package main
+
+import (
+	"github.com/MauroMeli23/api-supermarket/cmd/server/handler"
+	"github.com/MauroMeli23/api-supermarket/internal/domain"
+	"github.com/MauroMeli23/api-supermarket/internal/product"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var Products []domain.Product
+
+func main() {
+	// Cargar productos
+	loadedProducts, err := product.LoadProducts()
+	if err != nil {
+		panic(err)
+	}
+	Products = loadedProducts
+	// Inicializar el enrutador Gin
+	r := gin.Default()
+
+	// Definir las rutas
+	r.GET("/products", func(c *gin.Context) {
+		handler.GetAllProducts(c, Products)
+	})
+
+	r.GET("/products/:id", func(c *gin.Context) {
+		handler.GetProductByID(c, Products)
+	})
+
+	r.GET("/products/search", func(c *gin.Context) {
+		handler.GetProductByName(c, Products)
+	})
+
+	r.POST("/products", func(c *gin.Context) {
+		err := handler.AddNewProduct(c, &Products)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, gin.H{"message": "Producto añadido correctamente"})
+	})
+
+	// Iniciar el servidor
+	r.Run(":8080")
+}
